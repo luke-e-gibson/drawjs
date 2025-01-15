22d4b22bea7d7d38e0a9d5571ba97cbcf20c4453
luke-e-gibson <luke.e.gibson@outlook.com>
2025-01-15 22:30:32 +1100

    remove unneed wraped canvas

--- lib/react.tsx
+++ lib/react.tsx
@@ -11,19 +11,15 @@ export function Canvas(props: React.CanvasHTMLAttributes<HTMLCanvasElement>) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const draw = useDrawjs();
 
-  console.log('canvas', canvasRef.current);
+  console.log("canvas", canvasRef.current);
 
   useEffect(() => {
     if (canvasRef.current) {
       draw.attach(canvasRef.current);
     }
-  }, [canvasRef, draw])
+  }, [canvasRef, draw]);
 
-  return (
-    <div>
-        <canvas ref={canvasRef} {...props} />
-    </div>
-  );
+  return <canvas ref={canvasRef} {...props} />;
 }
 
 export function PenColorChanger(props: React.HTMLAttributes<HTMLInputElement>) {
@@ -31,13 +27,18 @@ export function PenColorChanger(props: React.HTMLAttributes<HTMLInputElement>) {
   const [color, setColor] = useState(draw.PenConfig.color);
 
   useEffect(() => {
-    draw.updatePenConfig({width: draw.PenConfig.width, color: color});
-  }, [color, draw])
+    draw.updatePenConfig({ width: draw.PenConfig.width, color: color });
+  }, [color, draw]);
 
   return (
-    <input type="color" onChange={(e)=> {setColor(e.target.value)}} {...props}/>
+    <input
+      type="color"
+      onChange={(e) => {
+        setColor(e.target.value);
+      }}
+      {...props}
+    />
   );
-
 }
 
 export function PenWidthChanger(props: React.HTMLAttributes<HTMLInputElement>) {
@@ -45,18 +46,34 @@ export function PenWidthChanger(props: React.HTMLAttributes<HTMLInputElement>) {
   const [width, setWidth] = useState(draw.PenConfig.width);
 
   useEffect(() => {
-    draw.updatePenConfig({width: width, color: draw.PenConfig.color});
-  }, [width, draw])
+    draw.updatePenConfig({ width: width, color: draw.PenConfig.color });
+  }, [width, draw]);
 
   return (
-    <input type="number" value={width} onChange={(e)=> {setWidth(Number(e.target.value))}} {...props}/>
+    <input
+      type="number"
+      value={width}
+      onChange={(e) => {
+        setWidth(Number(e.target.value));
+      }}
+      {...props}
+    />
   );
 }
 
-export function CanvasDebugButton(props: React.HTMLAttributes<HTMLButtonElement>) {
+export function CanvasDebugButton(
+  props: React.HTMLAttributes<HTMLButtonElement>
+) {
   const draw = useDrawjs();
 
   return (
-    <button onClick={() => {draw.toggleDebugPoints()}} {...props}>Debug</button>
-  )
+    <button
+      onClick={() => {
+        draw.toggleDebugPoints();
+      }}
+      {...props}
+    >
+      Debug
+    </button>
+  );
 }
