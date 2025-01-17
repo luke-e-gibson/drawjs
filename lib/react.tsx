75875cef2e1ef965eeee3819c0a811c09ac8f5da
luke-e-gibson <luke.e.gibson@outlook.com>
2025-01-17 19:18:09 +1100

    use css not tailwind

--- lib/react.tsx
+++ lib/react.tsx
@@ -33,8 +33,8 @@ export function Canvas(props: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div {...props}>
       <div style={{ position: "relative", width: "fit", height: "fit"}}>
-      <canvas  ref={canvasRef} style={{position: "absolute", left: "0", top: "0", zIndex: "1", border: "black 1px solid", width: "fit", height: "fit"}} id="front"/>
-      <canvas ref={backCanvas} style={{position: "absolute", left: "0", top: "0", zIndex: "0", border: "black 1px solid", width: "fit", height: "fit"}} id="back"/>
+      <canvas  ref={canvasRef} style={{position: "absolute", left: "0", top: "0", zIndex: "1", border: "black 1px solid", width: "%100", height: "%100"}} id="front"/>
+      <canvas ref={backCanvas} style={{position: "absolute", left: "0", top: "0", zIndex: "0", border: "black 1px solid", width: "%100", height: "%100"}} id="back"/>
     </div>
     </div>
   );
